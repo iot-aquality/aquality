8f0ee90a3b69b1a94119c516fca5cc0e4fed08d7
DanielM0802 <marillancadaniel@gmail.com>
2023-11-22 09:49:45 -0300

    dependencias de chartjs y creacion de grafico de lineas para calidad del agua

--- src/pages/Dahsboard.jsx
+++ src/pages/Dahsboard.jsx
@@ -1,6 +1,12 @@
+import WaterQuality from "../components/charts/WaterQuality"
+
 function Dahsboard() {
   return (
-    <div>Dahsboard</div>
+    <div>
+
+        <h1>Panel de control</h1>
+        <WaterQuality/>
+    </div>
   )
 }
 
